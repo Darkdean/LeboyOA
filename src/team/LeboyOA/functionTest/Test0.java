05aafe867df290be6d6384cca5476f03cbeadecd
Darkdean <2387088815@qq.com>
2018-01-26 12:17:32 +0800

    change

---
+++ src/team/LeboyOA/functionTest/Test0.java
@@ -0,0 +1,10 @@
+package team.LeboyOA.functionTest;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test ok");
+	}
+
+}
